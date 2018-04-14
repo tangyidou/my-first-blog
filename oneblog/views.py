ca889aaa11321349f5072175b4044409f360d848
tangyidou <tangyidou@foxmail.com>
2018-04-14 19:39:29 +0800

    加入了创建编辑博客文章的功能

--- oneblog/views.py
+++ oneblog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
+from oneblog.forms import ArticleForm
 from oneblog.models import Article
 
 
@@ -12,3 +13,32 @@ def article_list(request):
 def article_detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
     return render(request, 'oneblog/article_detail.html', {'article': article})
+
+
+def article_new(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.published_date = timezone.now()
+            article.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        form = ArticleForm()
+    return render(request, 'oneblog/article_edit.html', {'form': form})
+
+
+def article_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.published_date = timezone.now()
+            article.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, 'oneblog/article_edit.html', {'form': form})
